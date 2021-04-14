91e7b1faa14054a2cd7d0461a39cb06508ce0583
Emerson66 <emersonreisdacosta@gmail.com>
2021-04-14 17:59:08 -0400

    projeto criado

---
+++ src/main/java/com/buritiscript/springSecurityAPI/SpringSecurityApiApplication.java
@@ -0,0 +1,13 @@
+package com.buritiscript.springSecurityAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buritiscript/springSecurityAPI/SpringSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buritiscript.springSecurityAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
