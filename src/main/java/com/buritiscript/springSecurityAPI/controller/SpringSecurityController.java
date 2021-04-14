3f72a81aa52503ae6ec9e7cba0e399c304d70a69
Emerson66 <emersonreisdacosta@gmail.com>
2021-04-14 18:32:22 -0400

    criada a classe controladora

---
+++ src/main/java/com/buritiscript/springSecurityAPI/controller/SpringSecurityController.java
@@ -0,0 +1,22 @@
+package com.buritiscript.springSecurityAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class SpringSecurityController {
+
+	@GetMapping("/user")
+	public String helloUser() {
+		
+		return "Hello User";
+	}
+	
+	@GetMapping("/admin")
+	public String helloAdmin() {
+		
+		return "Hello Admin";
+	}
+}
