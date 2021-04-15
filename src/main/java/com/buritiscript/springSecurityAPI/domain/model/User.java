7b9f60517f48ce046013abdddd5725d4c0c1a4f9
Emerson66 <emersonreisdacosta@gmail.com>
2021-04-15 01:05:49 -0400

    mapeado a classe User e craida a classe Role

--- src/main/java/com/buritiscript/springSecurityAPI/domain/model/User.java
+++ src/main/java/com/buritiscript/springSecurityAPI/domain/model/User.java
@@ -1,17 +1,72 @@
 package com.buritiscript.springSecurityAPI.domain.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Collection;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Indexed;
+
+@Entity
+public class User implements UserDetails, Serializable{
 	
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String usuario;
 	private String senha;
+	
 	private String nomeCompleto;
 	
+	@CreatedDate
 	private LocalDate dataCriacao;
+	@LastModifiedDate
 	private LocalDate dataModificacao;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
