a7234df0daf4e403547bad4afd81de9f453f8356
Emerson66 <emersonreisdacosta@gmail.com>
2021-04-15 10:43:31 -0400

    criado a interface UserRepository

---
+++ src/main/java/com/buritiscript/springSecurityAPI/domain/model/UserRepository.java
@@ -0,0 +1,7 @@
+package com.buritiscript.springSecurityAPI.domain.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
