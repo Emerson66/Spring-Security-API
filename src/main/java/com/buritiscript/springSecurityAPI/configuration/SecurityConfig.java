5e548ed779a666e8b896487861c86a952cb6c556
Emerson66 <emersonreisdacosta@gmail.com>
2021-04-15 19:48:26 -0400

    criado as classes JwtTokenFilter we JwtTokenUtil (elas estão cheia de erros)

--- src/main/java/com/buritiscript/springSecurityAPI/configuration/SecurityConfig.java
+++ src/main/java/com/buritiscript/springSecurityAPI/configuration/SecurityConfig.java
@@ -1,21 +1,55 @@
 package com.buritiscript.springSecurityAPI.configuration;
 
+import static java.lang.String.format;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.buritiscript.springSecurityAPI.domain.repository.UserRepository;
+
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private final UserRepository userRepository;
+	
+//    private final JwtTokenFilter jwtTokenFilter;
+
+	
+	public SecurityConfig(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // TODO configure authentication manager
-    }
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO configure authentication manager
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // TODO configure web security
-    }
+		auth.userDetailsService(usuario -> userRepository
+	            .findByUsername(usuario)
+	            .orElseThrow(
+	                () -> new UsernameNotFoundException(
+	                    format("User: %s, not found", usuario)
+	                )
+	            ));
+	}
 	
+	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO configure web security
+	}
+
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
---
+++ src/main/java/com/buritiscript/springSecurityAPI/security/JwtTokenUtil.java
@@ -0,0 +1,75 @@
+package com.buritiscript.springSecurityAPI.security;
+
+import static java.lang.String.format;
+import java.security.SignatureException;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.buritiscript.springSecurityAPI.domain.model.User;
+
+@Component
+public class JwtTokenUtil {
+
+    private final String jwtSecret = "zdtlD3JK56m6wTTgsNFhqzjqP";
+    private final String jwtIssuer = "example.io";
+
+    private final Logger logger;
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .setSubject(format("%s,%s", user.getId(), user.getUsername()))
+                .setIssuer(jwtIssuer)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)) // 1 week
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUserId(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject().split(",")[0];
+    }
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject().split(",")[1];
+    }
+
+    public Date getExpirationDate(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            logger.error("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            logger.error("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            logger.error("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            logger.error("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            logger.error("JWT claims string is empty - {}", ex.getMessage());
+        }
+        return false;
+    }
+
+}
